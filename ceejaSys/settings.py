327760cf9570007db0ff5e2915399639ba24684d
Carlos Brito de Souza <ciclosdna@gmail.com>
2021-11-30 15:13:25 -0300

    perfil professor
    dados de perfil

--- ceejaSys/settings.py
+++ ceejaSys/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0h5rvoz-zw10zo@+&e)qf7gcws5-(thp9p6-3^_-h%yfxe9g+j
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost','127.0.0.1','192.168.1.7','186.241.49.19'
+    'localhost','127.0.0.1','192.168.1.10','186.241.49.19'
 ]
 
 
