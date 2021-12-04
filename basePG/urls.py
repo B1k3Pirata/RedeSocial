b66533d522ebd2e380c85fe0c30348a5c4050098
Carlos Brito de Souza <ciclosdna@gmail.com>
2021-12-04 14:18:10 -0300

    pouca alteracao
    detalhes

--- basePG/urls.py
+++ basePG/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from .views import Base, Landing
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
+app_name = "basePG"
+
 urlpatterns = [
     #path('', Base.as_view(), name='inicio'),
-    path('', Landing.as_view(), name='intro')
+    path('', Landing.as_view(), name='intro'),
+
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
 ]
