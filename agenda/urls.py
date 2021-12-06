b9aa4d1b3883c89db0d64f35841fe0a4ef7520d9
Carlos Brito de Souza <ciclosdna@gmail.com>
2021-12-06 18:47:03 -0300

    agenda_agendamento
    configurando parte1

--- agenda/urls.py
+++ agenda/urls.py
@@ -6,5 +6,8 @@ app_name = 'agenda'
 
 urlpatterns = [
     path('inicioagenda/', views.InicioAgenda.as_view(),name='inicioagenda'),
-
+    path('ag1/', AgendaNvl.as_view(), name='agenda_nivel'),
+    path('ag2/', AgendaDsc.as_view(), name='agenda_disciplina'),
+    path('ag3/', AgendaPrf.as_view(), name='agenda_professor'),
+    path('ok/', AgendaSucesso.as_view(), name='agenda_sucesso'),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -8,8 +8,58 @@ from agenda.models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 #@login_required
-def filtros(request):
-    pass
 
 class InicioAgenda(TemplateView):
     template_name = 'agenda/inicioagenda.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['titulo'] = 'Agendamento de Atividade'
+        context['subt'] = 'Bem-Vindo(a)!'
+        context['texto'] = 'Preencha os campos pedidos, para que posssamos melhor atende-lo(a)'
+
+        return context
+
+class AgendaNvl(CreateView):
+    model = Nivel
+    fields = '__all__'
+    template_name = 'agenda/form.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['titulo'] = 'Agendamento'
+        #context['texto1'] = 'Preencha seus dados completos, para melhor configuração do seu perfil'
+        context['botaoC'] = 'Avançar'
+        context['botaoB'] = 'Limpar'
+        context['botaoA'] = 'Cancelar'
+        return context
+    success_url = '/agenda/ag2/'
+
+class AgendaDsc(CreateView):
+    model = Disciplinas
+    fields = '__all__'
+    template_name = 'agenda/form.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['titulo'] = 'Agendamento de Disciplina'
+        #context['texto1'] = 'Preencha seus dados completos, para melhor configuração do seu perfil'
+        context['botaoC'] = 'Avançar'
+        context['botaoB'] = 'Limpar'
+        context['botaoA'] = 'Cancelar'
+        return context
+    success_url = '/agenda/ag3/'
+
+class AgendaPrf(CreateView):
+    model = Professor
+    fields = '__all__'
+    template_name = 'agenda/form.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['titulo'] = 'Agendamento com Professor'
+        #context['texto1'] = 'Preencha seus dados completos, para melhor configuração do seu perfil'
+        context['botaoC'] = 'Avançar'
+        context['botaoB'] = 'Limpar'
+        context['botaoA'] = 'Cancelar'
+        return context
+    success_url = '/agenda/ok/'
+
+class AgendaSucesso(TemplateView):
+    template_name = 'agenda/sucesso.html'
